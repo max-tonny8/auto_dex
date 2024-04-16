37cef89674c5bc8bb7cdcce416f41ad8a4f4324b
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-16 15:13:36 -0300

    feature: add 15 and 60 minutes exchange rate variation

--- _commons_/models/pool.ts
+++ _commons_/models/pool.ts
@@ -13,9 +13,19 @@ export class Pool {
     network: ChainId;
     price0: string;
     price0Change: number;
+    price0_15: string;
+    price0Change_15: number;
+    price0_60: string;
+    price0Change_60: number;
     price1: string;
     price1Change: number;
+    price1_15: string;
+    price1Change_15: number;
+    price1_60: string;
+    price1Change_60: number;
     lastUpdate: Date;
+    lastUpdate_15: Date;
+    lastUpdate_60: Date;
 
     constructor(pool: Pool) {
         this.id = pool.id;
@@ -30,9 +40,23 @@ export class Pool {
 
         this.price0 = pool.price0 || "0";
         this.price0Change = pool.price0Change || 0;
+        this.lastUpdate = pool.lastUpdate || new Date();
+
+        this.price0_15 = pool.price0_15 || "0";
+        this.price0Change_15 = pool.price0Change_15 || 0;
+        this.lastUpdate_15 = pool.lastUpdate_15 || new Date();
+
+        this.price0_60 = pool.price0_60 || "0";
+        this.price0Change_60 = pool.price0Change_60 || 0;
+        this.lastUpdate_60 = pool.lastUpdate_60 || new Date();
 
         this.price1 = pool.price1 || "0";
         this.price1Change = pool.price1Change || 0;
-        this.lastUpdate = pool.lastUpdate || new Date();
+
+        this.price1_15 = pool.price1_15 || "0";
+        this.price1Change_15 = pool.price1Change_15 || 0;
+
+        this.price1_60 = pool.price1_60 || "0";
+        this.price1Change_60 = pool.price1Change_60 || 0;
     }
 }
--- engine/src/repositories/poolsRepository.ts
+++ engine/src/repositories/poolsRepository.ts
@@ -38,7 +38,23 @@ async function addPool(pool: Pool): Promise<Pool> {
     return createdPool;
 }
 
-function buildSetPrice(pool: Pool, newPrice: number, tokenNumber: string) {
+function buildSet(newPrice: number, pool: any, tokenNumber: string, minutes: number) {
+    const setObj: any = {};
+    const xMinutesAgo = new Date(new Date().getTime() - (minutes * 60 * 1000)); // X minutes in the past
+
+    if(pool[`lastUpdate_${minutes}`] <= xMinutesAgo) {
+        const oldPriceX = Number(pool[`price${tokenNumber}_${minutes}`]);
+        const priceChangeX = ((newPrice - oldPriceX) / oldPriceX) * 100;
+
+        setObj[`price${tokenNumber}_${minutes}`] = `${newPrice}`;
+        setObj[`price${tokenNumber}Change_${minutes}`] = priceChangeX && Number.isFinite(priceChangeX) ? priceChangeX : 0;
+        setObj[`lastUpdate_${minutes}`] = new Date();
+    }
+
+    return setObj
+}
+
+function buildSetFull(pool: Pool, newPrice: number, tokenNumber: string) {
     if (!["0", "1"].includes(tokenNumber)) {
         throw new Error(`Invalid`);
     }
@@ -51,8 +67,10 @@ function buildSetPrice(pool: Pool, newPrice: number, tokenNumber: string) {
     setObj[`price${tokenNumber}Change`] = priceChange && Number.isFinite(priceChange) ? priceChange : 0;
     setObj.lastUpdate = new Date();
 
-    return setObj;
-    
+    const setObj_15 = buildSet(newPrice, pool, tokenNumber, 15);
+    const setObj_60 = buildSet(newPrice, pool, tokenNumber, 60);
+
+    return { ...setObj, ...setObj_15, ...setObj_60 };
 }
 
 async function updatePrices(poolData: PoolData): Promise<Pool | null> {
@@ -79,8 +97,8 @@ async function updatePrices(poolData: PoolData): Promise<Pool | null> {
     const newPrice0 = Number(poolData.token0Price);
     const newPrice1 = Number(poolData.token1Price);
 
-    const obj0 = buildSetPrice(pool, newPrice0, "0");
-    const obj1 = buildSetPrice(pool, newPrice1, "1");
+    const obj0 = buildSetFull(pool, newPrice0, "0");
+    const obj1 = buildSetFull(pool, newPrice1, "1");
 
     await db.pools.update({
         where: { id: poolData.id },
