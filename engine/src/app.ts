6eeded8c62b33cd6a201922a9d00ac81a1be2d23
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-16 11:02:56 -0300

    feature: v1 of executionCycle to update pools price from uniswap

--- engine/src/app.ts
+++ engine/src/app.ts
@@ -1,4 +1,27 @@
 import Config from './config';
+import poolsRepository from './repositories/poolsRepository';
 import { getTopPools } from './services/uniswapService';
 
-getTopPools().then(data => console.log(data));
+async function executionCycle() {
+    const pages = Math.ceil(Config.POOL_COUNT / 1000);
+
+    for(let i = 0; i < pages; i++) {
+        const pools = await getTopPools(1000, i * 1000);
+        console.log(`Loaded ${pools.length} pools...`);
+
+        for(let j = 0; j < pools.length; j++) {
+            const pool = pools[j];
+            const result = await poolsRepository.updatePrices(pool);
+
+            if(!result) continue;
+
+            console.log(`Price for ${result.symbol} (${result.fee / 10000}%) is ${Number(result.price0).toFixed(3)}`);
+        }
+    }
+}
+
+setInterval(executionCycle, Config.INTERVAL);
+
+executionCycle();
+
+console.log(`Execution cycle started at ${new Date().toISOString()}`);
