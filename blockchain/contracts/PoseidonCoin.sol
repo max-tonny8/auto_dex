9fe26ee95178be35a2b5d2ec98257cd7e64ce81c
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-22 17:35:35 -0300

    tests: adding unit tests and deploy

--- blockchain/contracts/PoseidonCoin.sol
+++ blockchain/contracts/PoseidonCoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PoseidonCoin is ERC20, Ownable {
-    constructor() Ownable(msg.sendeR) ERC20("PoseidonCoin", "PSC") {
+    constructor() Ownable(msg.sender) ERC20("PoseidonCoin", "PSC") {
         _mint(msg.sender, 100 * 10 ** decimals());
     }
 
--- blockchain/test/PoseidonPay.ts
+++ blockchain/test/PoseidonPay.ts
@@ -1,5 +1,6 @@
 import {
   loadFixture,
+  time,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
@@ -8,14 +9,89 @@ describe("PoseidonPay", function () {
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const PoseidonPay = await ethers.getContractFactory("Lock");
-    const poseidonPay = await PoseidonPay.deploy();
+    const PoseidonCoin = await ethers.getContractFactory("PoseidonCoin");
+    const poseidonCoin = await PoseidonCoin.deploy();
+    await poseidonCoin.waitForDeployment();
 
-    return { poseidonPay, owner, otherAccount };
+    const PoseidonPay = await ethers.getContractFactory("PoseidonPay");
+    const poseidonPay = await PoseidonPay.deploy();
+    await poseidonPay.waitForDeployment();
+    const poseidonAddress = await poseidonPay.getAddress();
+
+    await poseidonPay.setAcceptedToken(poseidonCoin.target);
+    await poseidonCoin.mint(otherAccount.address, ethers.parseEther("1"));
+
+    return { poseidonPay, poseidonAddress, poseidonCoin, owner, otherAccount };
   }
 
-  it("Should set the right unlockTime", async function () {
-    const {  poseidonPay } = await loadFixture(deployFixture);
-    expect(true).to.equal(true);
+  it("Should do first payment", async function () {
+    const { poseidonPay, poseidonAddress, poseidonCoin, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = poseidonCoin.connect(otherAccount);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.01"));
+
+    await expect(poseidonPay.pay(otherAccount.address)).to.emit(poseidonPay, "Paid");
+  });
+
+  it("Should not do first payment", async function () {
+    const { poseidonPay, otherAccount } = await loadFixture(deployFixture);
+
+    await expect(poseidonPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance and/or allowance");
+  });
+
+  it("Should do a second payment", async function () {
+    const { poseidonPay, poseidonAddress, poseidonCoin, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = poseidonCoin.connect(otherAccount);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.01"));
+
+    await poseidonPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60);
+
+    await expect(poseidonPay.pay(otherAccount.address)).to.emit(poseidonPay, "Paid");
+  });
+
+  it("Should not be able to do a second payment", async function () {
+    const { poseidonPay, poseidonAddress, poseidonCoin, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = poseidonCoin.connect(otherAccount);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.01"));
+
+    await poseidonPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.0001"));
+
+    await expect(poseidonPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance and/or allowance");
+  });
+
+  it("Should do a second payment after a failure", async function () {
+    const { poseidonPay, poseidonAddress, poseidonCoin, otherAccount } = await loadFixture(deployFixture);
+
+    const instance = poseidonCoin.connect(otherAccount);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.01"));
+    await poseidonPay.pay(otherAccount.address);
+
+    await time.increase(31 * 24 * 60 * 60);
+    await instance.approve(poseidonAddress, ethers.parseEther("0.0001"));
+    await expect(poseidonPay.pay(otherAccount.address)).to.be.revertedWith("Insufficient balance and/or allowance");
+
+    await instance.approve(poseidonAddress, ethers.parseEther("0.01"));
+    await expect(poseidonPay.pay(otherAccount.address)).to.emit(poseidonPay, "Paid");
+  });
+
+  it("Should withdraw", async function () {
+    const { poseidonPay, poseidonAddress, poseidonCoin, owner, otherAccount } = await loadFixture(deployFixture);
+
+    const monthlyAmount = await poseidonPay.monthlyAmount();
+    const instance = poseidonCoin.connect(otherAccount);
+    await instance.approve(poseidonAddress, monthlyAmount);
+    await poseidonPay.pay(otherAccount.address);
+
+    expect(await instance.balanceOf(poseidonAddress)).to.equal(monthlyAmount);
+
+    await poseidonPay.withdraw(monthlyAmount);
+    expect(await instance.balanceOf(poseidonAddress)).to.equal(0);
   });
 });
