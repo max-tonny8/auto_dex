bdb94d1071a114632e5ab051a79973bd6c04846d
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-23 12:05:08 -0300

    feature: create user endpoint in /auth/signup

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthDTO } from './auth.dto';
 import { UserDTO } from '../user/user.dto';
+import { UserService } from 'src/user/user.service';
+import { User } from 'commons/models/user';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
   @Post('/signin')
   signIn(@Body() data: AuthDTO): object {
     return data;
   }
 
   @Post('/signup')
-  signUp(@Body() data: UserDTO): object {
-    return data;
+  async signUp(@Body() data: UserDTO): Promise<User> {
+    const user = await this.userService.addUser(data);
+    // todo: send activation email
+    return user;
   }
 
   @Post('/activate/:wallet/:token')
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [],
+  providers: [UserService],
 })
 export class AuthModule {}
