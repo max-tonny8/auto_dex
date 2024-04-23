8c29ac3c915a6d044ff6aadc21bc5b4de48d804d
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-22 23:15:12 -0300

    chore: add cors and validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import Config from './config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(Config.PORT);
+
+  app.enableCors({
+    origin: Config.CORS_ORIGIN,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(Config.PORT, () =>
+    console.log(`Running backend on ${Config.PORT}`),
+  );
 }
 bootstrap();
