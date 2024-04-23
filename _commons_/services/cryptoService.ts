bf8c8a6b7f6c21f1e210042b008d76c70ec52c3a
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-23 16:03:31 -0300

    chore: AES encrypt and decrypt functions

---
+++ _commons_/services/cryptoService.ts
@@ -0,0 +1,22 @@
+import  aes from 'aes-js';
+import ConfigBase from '../configBase';
+
+const key = aes.utils.utf8.toBytes(ConfigBase.AES_KEY);
+
+if (key.length !== 32) {
+    throw new Error('Invalid key size for AES. It must have 256-bit / 32 bytes');
+}
+
+export function encrypt(text: string): string {
+    const bytesInfo = aes.utils.utf8.toBytes(text);
+    const aesCtr = new aes.ModeOfOperation.ctr(key);
+    const encryptedBytes = aesCtr.encrypt(bytesInfo);
+    return aes.utils.hex.fromBytes(encryptedBytes);
+}
+
+export function decrypt(encryptedHex: string): string {
+    const encryptedBytes = aes.utils.hex.toBytes(encryptedHex);
+    const aesCtr = new aes.ModeOfOperation.ctr(key);
+    const decryptedBytes = aesCtr.decrypt(encryptedBytes);
+    return aes.utils.utf8.fromBytes(decryptedBytes);
+}
