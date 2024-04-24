ce44eb9695da51a654434c791f9221c414a0ffa5
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-24 17:44:10 -0300

    chore: top statistics items

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,64 +1,15 @@
-import { DashboardCard } from "@/components/dashboard-card";
+import { DashboardCard } from "@/components/Dashboard/dashboard-card";
+import { StatisticTopCard } from "@/components/Dashboard/statistic-top-card";
 
 export default function Dashboard() {
     return (
       <div className="h-screen flex flex-1">
         <section className="bg-cyan-950 h-2/5 flex-1 flex items-center justify-between px-14">
-          <DashboardCard>
-            <div className="flex gap-3 flex-col font-light text-sm">
-              <div className="flex self-stretch justify-between items-start">
-                <div className="flex flex-col items-start">
-                  <h2 className="text-sm text-gray-500">TRAFFIC</h2>
-                  <span className="text-lg font-bold">350,897</span>
-                </div>
-                <div className="rounded-full bg-green-500 w-11 h-11 flex items-center justify-center">A</div>
-              </div>
+          <StatisticTopCard title="Traffic" value={350897} difference={3.48} />
+          <StatisticTopCard title="Traffic" value={2356} difference={-3.48} />
+          <StatisticTopCard title="Traffic" value={924} difference={-1.10} />
+          <StatisticTopCard title="Traffic" value={49.65} difference={12} />
 
-              <span className="text-gray-500">3,48% since last month</span>
-            </div>
-          </DashboardCard>
-
-          <DashboardCard>
-            <div className="flex gap-3 flex-col font-light text-sm">
-              <div className="flex self-stretch justify-between items-start">
-                <div className="flex flex-col items-start">
-                  <h2 className="text-sm text-gray-500">TRAFFIC</h2>
-                  <span className="text-lg font-bold">350,897</span>
-                </div>
-                <div className="rounded-full bg-green-500 w-11 h-11 flex items-center justify-center">A</div>
-              </div>
-
-              <span className="text-gray-500">3,48% since last month</span>
-            </div>
-          </DashboardCard>
-
-          <DashboardCard>
-            <div className="flex gap-3 flex-col font-light text-sm">
-              <div className="flex self-stretch justify-between items-start">
-                <div className="flex flex-col items-start">
-                  <h2 className="text-sm text-gray-500">TRAFFIC</h2>
-                  <span className="text-lg font-bold">350,897</span>
-                </div>
-                <div className="rounded-full bg-green-500 w-11 h-11 flex items-center justify-center">A</div>
-              </div>
-
-              <span className="text-gray-500">3,48% since last month</span>
-            </div>
-          </DashboardCard>
-
-          <DashboardCard>
-            <div className="flex gap-3 flex-col font-light text-sm">
-              <div className="flex self-stretch justify-between items-start">
-                <div className="flex flex-col items-start">
-                  <h2 className="text-sm text-gray-500">TRAFFIC</h2>
-                  <span className="text-lg font-bold">350,897</span>
-                </div>
-                <div className="rounded-full bg-green-500 w-11 h-11 flex items-center justify-center">A</div>
-              </div>
-
-              <span className="text-gray-500">3,48% since last month</span>
-            </div>
-          </DashboardCard>
         </section>
       </div>
     );
---
+++ frontend/src/components/Dashboard/statistic-top-card.tsx
@@ -0,0 +1,25 @@
+import { DashboardCard } from "./dashboard-card";
+
+type StatisticTopCardProps = {
+    title: string;
+    value: number;
+    difference: number;
+};
+
+export function StatisticTopCard({ title, value, difference}: StatisticTopCardProps) {
+    return (
+        <DashboardCard>
+            <div className="flex gap-3 flex-col font-light text-sm">
+            <div className="flex self-stretch justify-between items-start">
+                <div className="flex flex-col items-start">
+                <h2 className="text-sm text-gray-500">{title.toLocaleUpperCase()}</h2>
+                <span className="text-lg font-bold">{value}</span>
+                </div>
+                <div className="rounded-full bg-green-500 w-11 h-11 flex items-center justify-center">A</div>
+            </div>
+
+            <span className="text-gray-500">{difference}% since last month</span>
+            </div>
+      </DashboardCard>
+    );
+}
