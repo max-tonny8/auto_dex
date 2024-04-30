01b2447d9775baedd4663d856fc681c54ccc19dd
tonny0831 <tonnyjansen0831@gmail.com>
2024-04-30 19:04:27 -0300

    tests: UserService success cases

--- backend/test/user.service.mock.ts
+++ backend/test/user.service.mock.ts
@@ -14,3 +14,29 @@ export const newUserMock = {
   privateKey: 'abc123',
   status: Status.NEW,
 } as users;
+
+export const blockedUserMock = {
+  address: '0x123',
+  name: 'Caique Ribeiro',
+  email: 'caique@gmail.com',
+  id: 'abc123',
+  activationCode: '123456',
+  activationDate: new Date(),
+  network: ChainId.MAINNET,
+  planId: 'Gold',
+  privateKey: 'abc123',
+  status: Status.BLOCKED,
+} as users;
+
+export const activeUserMock = {
+  address: '0x123',
+  name: 'Caique Ribeiro',
+  email: 'caique@gmail.com',
+  id: 'abc123',
+  activationCode: '123456',
+  activationDate: new Date(),
+  network: ChainId.MAINNET,
+  planId: 'Gold',
+  privateKey: 'abc123',
+  status: Status.ACTIVE,
+} as users;
--- backend/test/user.service.spec.ts
+++ backend/test/user.service.spec.ts
@@ -1,7 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../src/user/user.service';
-import { newUserMock } from './user.service.mock';
+import {
+  activeUserMock,
+  blockedUserMock,
+  newUserMock,
+} from './user.service.mock';
 import { prismaMock } from './db.mock';
+import { Status } from 'commons/models/status';
+import { UserDTO } from 'src/user/user.dto';
 
 describe('UserService tests', () => {
   let userService: UserService;
@@ -18,10 +24,53 @@ describe('UserService tests', () => {
     expect(userService).toBeDefined();
   });
 
-  it('should get user by waller', async () => {
+  it('should get user by wallet', async () => {
     prismaMock.users.findFirst.mockResolvedValue({ ...newUserMock });
     const result = await userService.getUserByWallet(newUserMock.address);
     expect(result).toBeDefined();
     expect(result.address).toBe(newUserMock.address);
+  });
+
+  it('should get user by ID', async () => {
+    prismaMock.users.findUnique.mockResolvedValue({ ...newUserMock });
+    const result = await userService.getUser(newUserMock.address);
+    expect(result).toBeDefined();
+    expect(result.id).toBe(newUserMock.id);
+  });
+
+  it('should add user', async () => {
+    prismaMock.users.create.mockResolvedValue({ ...newUserMock });
+    const result = await userService.addUser(newUserMock);
+    expect(result).toBeDefined();
+    expect(result.id).toBe(newUserMock.id);
+  });
+
+  it('should pay user', async () => {
+    prismaMock.users.findFirst.mockResolvedValue({ ...blockedUserMock });
+    prismaMock.users.update.mockResolvedValue({ ...activeUserMock });
+    const result = await userService.payUser(blockedUserMock.address);
+    expect(result).toBeDefined();
+    expect(result.status).toBe(Status.ACTIVE);
+  });
+
+  it('should update user', async () => {
+    prismaMock.users.findFirst.mockResolvedValue({ ...activeUserMock });
+    prismaMock.users.update.mockResolvedValue({ ...activeUserMock });
+    const result = await userService.updateUser(newUserMock.id, {
+      ...activeUserMock,
+    } as UserDTO);
+    expect(result).toBeDefined();
+    expect(result.id).toBe(activeUserMock.id);
+  });
+
+  it('should activate user', async () => {
+    prismaMock.users.findFirst.mockResolvedValue({ ...newUserMock });
+    prismaMock.users.update.mockResolvedValue({ ...blockedUserMock });
+    const result = await userService.activateUser(
+      newUserMock.address,
+      newUserMock.activationCode,
+    );
+    expect(result).toBeDefined();
+    expect(result.status).toBe(Status.BLOCKED);
   });
 });
